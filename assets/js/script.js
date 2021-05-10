d027393793ad5856610d30f639a062ce04254853
Sompal Singh <sompal981@gmail.com>
2021-05-10 13:18:50 +0530

    Inital

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 
 /*Loader ends here*/
-$(document).ready(function(){
-    $(".loader-wrapper").delay(100).fadeIn('500', function(){
-        $('.loader-wrapper').delay(100).fadeOut('500');
-        $('#loader').delay(100).fadeOut('500');
-    });
-});
+// $(document).ready(function(){
+//     $(".loader-wrapper").fadeIn('100', function(){
+//         $('.loader-wrapper').delay(100).fadeOut('100');
+//         $('#loader').fadeOut('100');
+//     });
+// });
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
